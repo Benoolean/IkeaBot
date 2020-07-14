2e59a100fec10edc172b001074b2e60c0e803114
Benoolean <benxiao778@gmail.com>
2020-07-13 23:29:04 -0400

    Product regression refactor

--- core_scrape/ikea_scraper/ikea_spider.py
+++ core_scrape/ikea_scraper/ikea_spider.py
@@ -2,6 +2,8 @@ import scrapy
 import os
 import logging
 import pandas as pd
+import urllib.request
+import json 
 from twisted.internet import reactor, defer
 from scrapy.crawler import CrawlerRunner
 from csv import reader
@@ -25,57 +27,39 @@ class IkeaCategoriesSpider(scrapy.Spider):
     def parse(self, response):
         # creating/overwriting the product list with the columns of both
         # the product and category files
-        ikea_category_df_schema_data = {
-            'category-id': [],
-            'category_url': ''
+        ikea_category_df_data = {
+            'category_id': [],
+            'category_name': []
         }
         
-        ikea_category_df = pd.DataFrame(data=ikea_category_df_schema_data, dtype=str)
         category_list = enumerate(response.css('.vn-accordion__item > ul > li > a::attr(href)').getall())
 
         for index, category_url in category_list:
             # Getting the last array element in the stripped URL
             # .strip('/') removes the trailing / in the URL
-            category_id = category_url.strip('/').split('-')[-1]
-            ikea_category_df.loc[index] = [category_id, category_url]
-        
-        print(ikea_category_df)
+            category_name_id = category_url.strip('/').split('/')[-1]
+            category_name = ' '.join(category_name_id.split('-')[:-1]).title()
+            category_id = category_name_id.split('-')[-1]
+
+            ikea_category_df_data['category_id'] += [category_id]
+            ikea_category_df_data['category_name'] += [category_name]
+
+        ikea_category_df = pd.DataFrame(data=ikea_category_df_data, dtype=str)
         ikea_category_df.to_csv(categories_csv, encoding='utf-8', index=False)
 
         print('[CONSOLE]: Crawled categories and exported CSV files')
 
+        # getting products
 
-class IkeaProductsSpider(scrapy.Spider):
-    name = 'ikea_products'
+        for index, row in ikea_category_df.iterrows():
+            if index < 1:
+                product_search_url = 'https://sik.search.blue.cdtapps.com/ca/en/product-list-page/more-products?category={category_id}&start=0&end=99999999'
+                product_search_url = product_search_url.format(category_id=row['category_id'])
+                with urllib.request.urlopen(product_search_url) as req_data:
+                    data = json.loads(req_data.read().decode())
+                    df = pd.DataFrame.from_dict(data['moreProducts']['productWindow'])
 
-    def start_requests(self):
-        with open(categories_csv) as f:
-            categories_csv_urls = reader(f)
-            # skip the header
-            header = next(categories_csv_urls)
-            
-            if header != None:
-                for url in categories_csv_urls:
-                    # url <list(str)> into str
-                    url = url[0]
-                    yield scrapy.Request(url=url, callback=self.parse)
-
-            f.close()
-
-
-    def parse(self, response):
-        category = response.css('title::text').get().replace('- IKEA', '').replace(',', '|').strip()
-
-        # Links of all the product with url argument
-        product_list = response.css('.range-revamp-product-compact > a::attr(href)').getall()
-
-        with open(products_csv, 'a', encoding="utf-8") as f:
-            for url in product_list:
-                f.write('{category},{url}\n'.format(category=category, url=url))
-
-            f.close()
-    
-    print('[CONSOLE]: Crawled products and exported CSV files')
+                    print(df)
 
 logging.disable(20) # 20 is infomation logging
 
@@ -83,7 +67,6 @@ runner = CrawlerRunner()
 @defer.inlineCallbacks
 def crawl():
     yield runner.crawl(IkeaCategoriesSpider)
-    # yield runner.crawl(IkeaProductsSpider)
     reactor.stop()
 
 crawl()
